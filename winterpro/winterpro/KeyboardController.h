2f7b0eee65d3cce6d60eba0b00307992fac35034
MSI\banba <banban.n.namchan@gmail.com>
2021-10-22 14:07:24 +0900

    KeyboardControllerの内部処理追加

--- winterpro/winterpro/KeyboardController.h
+++ winterpro/winterpro/KeyboardController.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef KEYBOARD_CONTROLLER_H_
+#define KEYBOARD_CONTROLLER_H_
+
+class KeyboardController
+{
+public:
+	struct KeyState
+	{
+		//キー情報　=> 押された or 押されていない
+		bool IsPrevPushed;        //1フレーム前のキー情報
+		bool IsCurrentPushed;     //今のフレームのキー情報
+	};
+
+	static const int MaxKey = 256;
+
+public:
+	KeyboardController();
+	void Update();
+	bool IsPushed(int key);
+	bool IsHeld(int key);
+	bool IsReleased(int key);
+
+public:
+	KeyState KeyStates[MaxKey];
+};
+
+#endif // !KEYBOARD_CONTROLLER_H_
